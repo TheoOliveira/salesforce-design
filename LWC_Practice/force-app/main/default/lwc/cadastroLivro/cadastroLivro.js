73e7d2f42372e3ef8a6e44414f4757c21153bdc8
TheoOliveira <akai.hen@gmail.com>
2020-06-05 09:43:52 -0300

    correcao lwc

--- LWC_Practice/force-app/main/default/lwc/cadastroLivro/cadastroLivro.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class CadastroLivro extends LightningElement {}
--- LWC_Practice/force-app/main/default/lwc/emprestimoDevolucao/emprestimoDevolucao.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class EmprestimoDevolucao extends LightningElement {}
