08a9fc6f6acca791d5b41a5129dd76c76552ab5d
TheoOliveira <akai.hen@gmail.com>
2020-06-10 16:57:50 -0300

    exercicio expenses sem a challenge

--- LWC_Practice/force-app/main/default/aura/campingList/campingListController.js
+++ LWC_Practice/force-app/main/default/aura/campingList/campingListController.js
@@ -1,4 +1,22 @@
 ({
+    doInit: function(component, event, helper) {
+      // call apex controller
+      const action = component.get("c.getItems");
+  
+      //set callback
+      action.setCallback(this, response => {
+        let state = response.getState();
+        if (state === "SUCCESS") {
+          console.log(state);
+          component.set("v.items", response.getReturnValue());
+        } else {
+          console.log("Failed:" + state);
+        }
+      });
+      $A.enqueueAction(action);
+    }
+  },
+    {
   clickCreateItem: function(component, event, helper) {
     let validItem = component
       .find("itemform")
@@ -12,27 +30,10 @@
       // Create the new expense
       let item = component.get("v.newItem");
       helper.createItem(component, item);
-      //let defaultItem = {'sobjectType': 'Camping_Item__c', 'Name': '', 'Quantity__c': 0, 'Price__c':0, 'Packed__c' : false };
-      //component.set('v.newItem', defaultItem);
+      let defaultItem = {'sobjectType': 'Camping_Item__c', 'Name': '', 'Quantity__c': 0, 'Price__c':0, 'Packed__c' : false };
+      component.set('v.newItem', defaultItem);
     }
   }
 });
 
-({
-  doInit: function(component, event, helper) {
-    // call apex controller
-    const action = component.get("c.getItems");
 
-    //set callback
-    action.setCallback(this, response => {
-      let state = response.getState();
-      if (state === "SUCCESS") {
-        console.log(state);
-        component.set("v.items", response.getReturnValue());
-      } else {
-        console.log("Failed:" + state);
-      }
-    });
-    $A.enqueueAction(action);
-  }
-});
--- LWC_Practice/force-app/main/default/aura/expenses/expensesController.js
+++ LWC_Practice/force-app/main/default/aura/expenses/expensesController.js
@@ -1,35 +1,26 @@
-(
-    
-    {
-    clickCreate: function(component, event, helper) {
-        const validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        // If we pass error checking, do some real work
-        if(validExpense){
-            // Create the new expense
-            const expense = component.get("v.newExpense");
-            helper.createExpense(component, expense);
-        }
-    }
-})
-
 ({
-doInit: function(component, event, helper){
-    const action = component.get('c.getExpenses');
-    
-    //set callback
-    action.setCallback(this, (response)=>{
-        let state = response.getState();
-        if(state === "SUCCESS"){
-            component.set('v.expenses', response.getReturnValue());
-        }
-        else{
-            console.log("Failed with state: " +state);
-        } 
+  doInit: function(component, event, helper) {
+    // Create the action
+    const action = component.get("c.getExpenses");
+    // Add callback behavior for when response is received
+    action.setCallback(this, response => {
+      let state = response.getState();
+      if (state === "SUCCESS") {
+        component.set("v.expenses", response.getReturnValue());
+      } else {
+        console.log("Failed with state: " + state);
+      }
     });
-
+    // Send action off to be executed
     $A.enqueueAction(action);
-}})
+  },
+  handleCreateExpense: function(component, event, helper) {
+    var newExpense = event.getParam("expense");
+    helper.createExpense(component, newExpense);
+  },
+
+  handleUpdateExpense: function(component, event, helper) {
+    var updatedExp = event.getParam("expense");
+    helper.updateExpense(component, updatedExp);
+  }
+});
